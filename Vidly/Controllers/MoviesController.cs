0cd79b5cbbe173e02eb0bcaac4e0076a44f37182
btang <btang@BERNARDTANG6841>
2019-08-20 19:30:47 +1000

    Section 2 Exercise : Add Movies page.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -34,13 +34,15 @@ namespace Vidly.Controllers
             return Content("id=" + id);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Index()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (string.IsNullOrEmpty(sortBy))
-                sortBy = "Name";
-            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            var movies = new List<Movie>()
+            {
+                new Movie() { Id = 1, Name = "Shrek!" },
+                new Movie() { Id = 2, Name = "Wall-e" }
+            };
+
+            return View(movies);
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
