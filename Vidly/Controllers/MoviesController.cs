02507fc6d33cfde02117189d8e0b6be7bcd6ec3e
btang <btang@BERNARDTANG6841>
2019-08-22 06:35:18 +1000

    Section 3 : Exericse (Included GenereType in Index page)

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,11 +5,24 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ApplicationDbContext _context { get; set; }
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -36,11 +49,13 @@ namespace Vidly.Controllers
 
         public ActionResult Index()
         {
-            var movies = new List<Movie>()
-            {
-                new Movie() { Id = 1, Name = "Shrek!" },
-                new Movie() { Id = 2, Name = "Wall-e" }
-            };
+            //var movies = new List<Movie>()
+            //{
+            //    new Movie() { Id = 1, Name = "Shrek!" },
+            //    new Movie() { Id = 2, Name = "Wall-e" }
+            //};
+
+            var movies = _context.Movies.Include(m => m.GenreType).ToList();
 
             return View(movies);
         }
