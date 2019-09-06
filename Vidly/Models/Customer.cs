ef7f640d58d2a7eb1e68971271de286ac299f5d3
btang <btang@BERNARDTANG6841>
2019-09-07 00:35:04 +1000

    Section 4.40 : Drop-down Lists

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -8,12 +8,18 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsLetter { get; set; }
+
         public MembershipType MembershipType { get; set; }      // aka Navigation Property
+
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
+
         [Display(Name = "Date of Birth")]
         public DateTime? DOB { get; set; }
     }
