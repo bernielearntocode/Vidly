3aec1c847e34095c37c7d586bb7fc771f8803134
btang <btang@BERNARDTANG6841>
2019-09-07 00:43:10 +1000

    Section 4.41 : Model Binding

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -34,6 +34,12 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        [HttpPost]
+        public ActionResult Create(NewCustomerViewModel viewModel)
+        {
+            return View();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
